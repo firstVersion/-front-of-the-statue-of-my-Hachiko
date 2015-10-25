6f5ee17bf3e81bd4f89a854108fced36d0975972
firstVersion <kuroisiratama@yahoo.co.jp>
2015-10-25 21:19:11 +0900

    構造修正

---
+++ server.js
@@ -0,0 +1,23 @@
+var server = require('http').createServer(handler),
+	io = require('socket.io').listen(server),
+	fs = require('fs'),
+	set= require('./settings');
+
+server.listen(set.port, set.host);
+console.log("->"+set.host+":"+set.port);
+
+function handler(req,res)
+{
+	console.log("connection!");
+	fs.readFile(__dirname + '/public_html/index.html',function(err,data)
+	{
+		if(err)
+		{
+			res.writeHead(500);
+			return res.end('Error');
+		}
+		res.writeHead(200);
+		res.write(data);
+		res.end();
+	});
+}
---
+++ settings.js
@@ -0,0 +1,3 @@
+exports.port = 1377;
+exports.host = '192.168.33.10';
+exports.db   = 'nodedb';
