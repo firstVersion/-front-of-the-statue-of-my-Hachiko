1dd10c7808084262b986cd7a24cfaaa47caabd3a
firstVersion <kuroisiratama@yahoo.co.jp>
2015-10-28 20:23:31 +0900

    mimetypeto

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var server 	= require('http').createServer(handler),
 	url 	= require('url'),
 	set 	= require('./settings');
 
-server.listen(set.port, set.host);
-console.log("-> "+set.host+":"+set.port);
+(function(path,host,host){
+
+server.listen(port, host);
+console.log("-> "+host+":"+port);
+console.log("path-> "+ path);
 
 function handler(req,res)
 {	
@@ -21,37 +24,19 @@ function handler(req,res)
 	fs.stat(filePath, function(err, stats){
 		if(err)
 		{
-			if((/ENOENT/).test(err.message))return sendError(req,res,404);
+			if( (/ENOENT/).test(err.message) ) return sendError(req,res,404);
 			else return sendError(req,res,500);
 		}
 		if(!stats.isDirectory())
 			return sendFile(req,res,filePath);
 	});
-
-}
-
-var message = {
-200: "OK",
-404: "Not Found",
-500: "Internal Server Error",
-501: "Note Implemented"
-}
-
-var mime = {
-".html": "text/html",
-".css" : "text/css",
-".js"  : "application/javascript",
-".png" : "image/png",
-".jpg" : "image/jpeg",
-".gif" : "image/gif",
-".txt" : "text/plain"
 }
 
 function sendFile(req, res, filePath)
 {
 	var file = fs.createReadStream(filePath);
 	file.on("readble", function(){
-		res.writeHead(200,{"Content-Type":mime[path.extname(filePath)] || "text/plain"});
+		res.writeHead(200,{"Content-Type":set.mime[path.extname(filePath)] || "text/plain"});
 	}).on("data", function(data){
 		res.write(data);
 	}).on("error", function(err){
@@ -59,13 +44,23 @@ function sendFile(req, res, filePath)
 	})
 }
 
+function sentFile2(req,res,filePath)
+{
+	switch(path.extname(filePath))
+	{
+		case ".css": break;
+		case ".js" : break;
+		default:break;
+	}
+}
+
 function sendError(req,res,statusCode)
 {
 	res.writeHead(statusCode, {"Content-Type":"text/html"});
-	res.write("<!DOCTYPE html><html><head></head>"+message[statusCode]+"<body></body></html>")
+	res.write("<!DOCTYPE html><html><head></head>"+set.message[statusCode]+"<body></body></html>")
 	res.end();
 }
 
-
+})( process.argv[2]||"./", process.argv[3]||set.port, process.argv[4]||set.host );
 
 
--- settings.js
+++ settings.js
@@ -1,3 +1,18 @@
 exports.port = 1377;
 exports.host = '192.168.33.10';
 exports.db   = 'nodedb';
+exports.message = {
+200: "OK",
+404: "Not Found",
+500: "Internal Server Error",
+501: "Note Implemented"
+}
+exports.mime = {
+".html": "text/html",
+".css" : "text/css",
+".js"  : "application/javascript",
+".png" : "image/png",
+".jpg" : "image/jpeg",
+".gif" : "image/gif",
+".txt" : "text/plain"
+}
