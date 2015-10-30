9f3ac4874f681694ed0034e42dcb04f109cdb17e
firstVersion <kuroisiratama@yahoo.co.jp>
2015-10-30 05:34:10 +0000

    途中経過

--- server.js
+++ server.js
@@ -1,66 +1,70 @@
-var server 	= require('http').createServer(handler),
-	io 		= require('socket.io').listen(server),
+var server 	= require('http'),
+	io 		= require('socket.io'),
 	fs 		= require('fs'),
 	path 	= require('path'),
 	url 	= require('url'),
 	set 	= require('./settings');
 
-(function(path,host,host){
 
-server.listen(port, host);
-console.log("-> "+host+":"+port);
-console.log("path-> "+ path);
+	function handler(req,res)
+	{	
+		var filePath;
+		var pathName = url.parse(req.url).pathname;
 
-function handler(req,res)
-{	
-	var filePath;
-	var pathName = url.parse(req.url).pathname;
 
-	if(req.method === "GET")
-		filePath = path.join(root, pathName);
-	else
-		return sendError(req,res,501);
-		
-	fs.stat(filePath, function(err, stats){
-		if(err)
-		{
-			if( (/ENOENT/).test(err.message) ) return sendError(req,res,404);
-			else return sendError(req,res,500);
-		}
-		if(!stats.isDirectory())
-			return sendFile(req,res,filePath);
-	});
-}
+		if(req.method === "GET")
+			filePath = path.join(set.root, pathName);
+		else
+			return sendError(req,res,501);
 
-function sendFile(req, res, filePath)
-{
-	var file = fs.createReadStream(filePath);
-	file.on("readble", function(){
-		res.writeHead(200,{"Content-Type":set.mime[path.extname(filePath)] || "text/plain"});
-	}).on("data", function(data){
-		res.write(data);
-	}).on("error", function(err){
-		sendError(req,res,500);
-	})
-}
+		fs.stat(filePath, function(err, stats){
 
-function sentFile2(req,res,filePath)
-{
-	switch(path.extname(filePath))
-	{
-		case ".css": break;
-		case ".js" : break;
-		default:break;
+			console.log("error",err);
+			
+			if(err)
+			{
+				if( (/ENOENT/).test(err.message) ) return sendError(req,res,404);
+				else return sendError(req,res,500);
+			}
+			if(!stats.isDirectory())
+				return sendFile(req,res,filePath);
+		});
+
+		console.log(req.method,filePath);
 	}
-}
 
-function sendError(req,res,statusCode)
-{
-	res.writeHead(statusCode, {"Content-Type":"text/html"});
-	res.write("<!DOCTYPE html><html><head></head>"+set.message[statusCode]+"<body></body></html>")
-	res.end();
-}
+	  function sendFile(req, res, filePath) {
 
-})( process.argv[2]||"./", process.argv[3]||set.port, process.argv[4]||set.host );
+	    var file = fs.createReadStream(filePath);
+	    file.on("readable", function() {
+	      res.writeHead(200, {"Content-Type":set.mime[path.extname(filePath)] || "text/plain"});
+	    });
+
+	    file.on("data", function(data) {
+	      res.write(data);
+	      console.log(res);
+	    });
+
+	    file.on("close", function() {
+	      res.end();
+	      console.log("<- " + set.message[200] + ": " + req.method + " " + req.url);
+	    });
+
+	    file.on("error", function(err) {
+	      sendError(req, res, 500);
+	    });
+	  }
+
+	function sendError(req,res,statusCode)
+	{
+		res.writeHead(statusCode, {"Content-Type":"text/html"});
+		res.write("<!DOCTYPE html><html><head></head>"+set.message[statusCode]+"<body></body></html>")
+		res.end();
+	}
+
+(function(port,host){
+	server.listen(port, host);
+	console.log("-> "+host+":"+port);
+})( process.argv[3]||set.port, process.argv[4]||set.host );
 
 
--- settings.js
+++ settings.js
@@ -1,5 +1,6 @@
 exports.port = 1377;
 exports.host = '192.168.33.10';
+exports.root = "/vagrant/html/ore8/public_html/";
 exports.db   = 'nodedb';
 exports.message = {
 200: "OK",
